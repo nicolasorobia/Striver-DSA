cfc5d277c678f90d6316ca0a2fa6996a99b7d7ba
Nicolas <nicolasorobia@gmail.com>
2026-05-22 14:32:57 -0700

    Refactor palindrome check functions and add leetcode-style validation

--- Basics/Recursion/IsStringPalindrome.cpp
+++ Basics/Recursion/IsStringPalindrome.cpp
@@ -3,18 +3,60 @@
 using namespace std;
 
 bool palindromeCheck(string& s) {
+    int p1 = 0;
+    int p2 = s.size() - 1;
 
-    
+    while (p1 < p2) {
+        if(s[p1] != s[p2]) return false;
+        p1++;
+        p2--;
+    }
 
+    return true;
+}
 
+bool recursivePalindromeCheck(string& s, int start, int end) {
 
-    return false;
+    if (start >= end) return true;
+
+    if (s[start] != s[end]) return false;
+
+    return recursivePalindromeCheck(s, start + 1, end - 1);
+}
+
+bool leetcodePalindromeCheck(string s) {
+    string valid_s = "";
+
+    // convert string by making all letters lowercase & removing non-alphanumeric characters:
+    // test case example: "A man, a plan, a canal: Panama"
+    for (auto c: s) {
+        if (isalnum(c)) valid_s += tolower(c);
+    }
+
+    return palindromeCheck(valid_s);
 }
 
 int main() {
 
-    string s1 = "Hannah"; // expected: true
-    string s2 = "Bandit"; // expected: false
+    string s1 = "hannah"; // expected: true
+    string s2 = "bandit"; // expected: false
+    string s3 = "aAaA";   // expected: false
+
+    string s4 = "A man, a plan, a canal: Panama";
+
+    cout << boolalpha;
+    cout << palindromeCheck(s1) << endl;
+    cout << palindromeCheck(s2) << endl;
+    cout << palindromeCheck(s3) << endl;
+    
+    cout << "\nChecks Palindrome with recursion:\n";
+    cout << recursivePalindromeCheck(s1, 0, s1.size() - 1) << endl; // true
+    cout << recursivePalindromeCheck(s2, 0, s3.size() - 1) << endl; // false
+    cout << recursivePalindromeCheck(s3, 0, s3.size() - 1) << endl; // false
+
+    cout << "\nLeetcode Test case sample:\n";
+    cout << leetcodePalindromeCheck(s4) << endl;
 
 
+    return 0;
 }
