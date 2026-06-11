fe3d9e2f4979af7b4a7899da533321f541315168
Nicolas <nicolasorobia@gmail.com>
2026-06-11 15:27:48 -0700

    add recursive bubble sort logic

--- Sorting/RecursiveBubbleSort.cpp
+++ Sorting/RecursiveBubbleSort.cpp
@@ -2,12 +2,28 @@
 #include <vector>
 using namespace std;
 
-vector<int> bubbleSort(vector<int>& nums) {
+vector<int> bubbleSortRecursion(vector<int>& nums, int size) {
+    // base case
+    if (size == 0) return nums;
 
+    for (int i = 0; i < size - 1; i++) {
+        if (nums[i] > nums[i+1]) {
+            swap(nums[i], nums[i+1]);
+        }
+    }
+
+    return bubbleSortRecursion(nums, size - 1);
 }
 
 int main() {
 
+    vector<int> nums {7,4,1,5,3};
+
+    vector<int> ans = bubbleSortRecursion(nums, nums.size());
+
+    for (auto& num: ans) {
+        cout << num << " ";
+    }
 
     return 0;
 }
